786c5f9786dd48f1abd7f38e630528e276fbb864
driss292 <drisskaci@outlook.fr>
2022-10-03 10:43:25 +0200

    form

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 require_once("database/connection.php");
 $title = "Upload index";
+if (isset($_POST["add-image"])) {
+    var_dump($_FILES["photo"]);
+    $data = [
+        "title" => htmlentities($_POST["title"]),
+
+    ];
+}
 ?>
 
 <!DOCTYPE html>
@@ -15,7 +22,24 @@ $title = "Upload index";
 </head>
 
 <body>
-    <div class="container"></div>
+    <div class="container">
+        <div class="add-images">
+            <h1>Ajouter une image</h1>
+            <div class="add-images__form">
+                <form action="" method="post" enctype="multipart/form-data">
+                    <div>
+                        <label for="title">Nom de la photo</label>
+                        <input type="text" name="title" id="title">
+                    </div>
+                    <div>
+                        <label for="photo">Choisir une photo</label>
+                        <input type="file" accept="image/png, image/jpeg" name="photo" id="photo">
+                    </div>
+                    <button type="submit" name="add-image">Envoyer la photo</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
